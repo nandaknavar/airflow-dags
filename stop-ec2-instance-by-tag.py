d9dfabdf81b6a8a93459230115337d7b0523c185
Nanda Navarathnam <nandaknavar@gmail.com>
2023-09-21 22:11:45 -0400

    start/stop ec2 instances on schedule

---
+++ stop-ec2-instance-by-tag.py
@@ -0,0 +1,49 @@
+# Airflow DAG to stop a set of EC2 instances on a schedule
+# This DAG is part of scheduled stop and start instances to save costs
+# Airflow version 2.4.3
+# apache-airflow-providers-amazon version 7.2.1
+
+from pathlib import Path
+
+import pendulum
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.amazon.aws.operators.ec2 import EC2StopInstanceOperator
+
+default_args = {
+    "owner": "nandaknavar",
+    "start_date": pendulum.datetime(2023, 1, 1, tz="US/Eastern"),
+    "depends_on_past": False,
+    "email_on_failure": True,
+}
+
+
+@task
+def get_instances():
+    import boto3
+
+    ec2 = boto3.client("ec2")
+    response = ec2.describe_instances(
+        Filters=[{"Name": "tag:schedule-start-stop", "Values": ["yes"]}]
+    )
+
+    instances = (
+        instance_id
+        for reservation in response["Reservations"]
+        for instance_id in reservation["Instances"]
+    )
+
+    return [instance["InstanceId"] for instance in instances]
+
+
+with DAG(
+    Path(__file__).stem,
+    schedule="00 18 * * *",
+    description="Lookup EC2 instances by tag and stop them on schedule",
+    default_args={**default_args, "email": Variable.get("alert_email")},
+    catchup=False,
+    tags=["it-operations"],
+):
+    stop_instance = EC2StopInstanceOperator.partial(task_id="stop_instance").expand(
+        instance_id=get_instances()
+    )
